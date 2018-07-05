7bb44e7638a970a3474df356363575adb6974607
Vladimir Elkin <elkin.v.n@gmail.com>
2018-07-05 19:08:20 +0300

    Add new boost test facace

--- pytest_cpp/boost.py
+++ pytest_cpp/boost.py
@@ -4,7 +4,6 @@ import re
 import shutil
 import subprocess
 import tempfile
-import pkg_resources
 from xml.etree import ElementTree
 
 from pytest_cpp.error import CppTestFailure
@@ -14,9 +13,6 @@ class BoostTestFacade(object):
     """
     Facade for BoostTests.
     """
-
-    _BOOST_VERSION = re.compile(r'Boost\s*:\s*(?P<version>\d+\.\d+.\d+)\s*')
-    _MIN_BOOST_VERSION_WITH_XML_SUPPORT = pkg_resources.parse_version('1.63.0')
 
     @classmethod
     def is_test_suite(cls, executable):
@@ -34,19 +30,116 @@ class BoostTestFacade(object):
         # inside the executable, so the test_id is a dummy placeholder :(
         return [os.path.basename(os.path.splitext(executable)[0])]
 
+   def run_test(self, executable, test_id, test_args=()):
+
+        def read_file(name):
+            try:
+                with io.open(name) as f:
+                    return f.read()
+            except IOError:
+                return None
+
+        temp_dir = tempfile.mkdtemp()
+        log_xml = os.path.join(temp_dir, 'log.xml')
+        report_xml = os.path.join(temp_dir, 'report.xml')
+        args = [
+            executable,
+            '--output_format=XML',
+            '--log_sink=%s' % log_xml,
+            '--report_sink=%s' % report_xml,
+        ]
+        args.extend(test_args)
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, _ = p.communicate()
+
+        log = read_file(log_xml)
+        report = read_file(report_xml)
+
+        if p.returncode not in (0, 200, 201):
+            msg = ('Internal Error: calling {executable} '
+                   'for test {test_id} failed (returncode={returncode}):\n'
+                   'output:{stdout}\n'
+                   'log:{log}\n'
+                   'report:{report}')
+            failure = BoostTestFailure(
+                '<no source file>',
+                linenum=0,
+                contents=msg.format(executable=executable,
+                                    test_id=test_id,
+                                    stdout=stdout,
+                                    log=log,
+                                    report=report,
+                                    returncode=p.returncode))
+            return [failure]
+
+        if report is not None and (
+                report.startswith('Boost.Test framework internal error: ') or
+                report.startswith('Test setup error: ')):
+            # boost.test doesn't do XML output on fatal-enough errors.
+            failure = BoostTestFailure('unknown location', 0, report)
+            return [failure]
+
+        results = self._parse_log(log=log)
+        shutil.rmtree(temp_dir)
+        if results:
+            return results
+
+    def _parse_log(self, log):
+        """
+        Parse the "log" section produced by BoostTest.
+
+        This is always a XML file, and from this we produce most of the
+        failures possible when running BoostTest.
+        """
+        # Fatal errors apparently generate invalid xml in the form:
+        # <FatalError>...</FatalError><TestLog>...</TestLog>
+        # so we have to manually split it into two xmls if that's the case.
+        parsed_elements = []
+        if log.startswith('<FatalError'):
+            fatal, log = log.split('</FatalError>')
+            fatal += '</FatalError>'  # put it back, removed by split()
+            fatal_root = ElementTree.fromstring(fatal)
+            fatal_root.text = 'Fatal Error: %s' % fatal_root.text
+            parsed_elements.append(fatal_root)
+
+        log_root = ElementTree.fromstring(log)
+        parsed_elements.extend(log_root.findall('Exception'))
+        parsed_elements.extend(log_root.findall('Error'))
+        parsed_elements.extend(log_root.findall('FatalError'))
+
+        result = []
+        for elem in parsed_elements:
+            filename = elem.attrib['file']
+            linenum = int(elem.attrib['line'])
+            result.append(BoostTestFailure(filename, linenum, elem.text))
+        return result
+
+
+class NewBoostTestFacade(object):
+    """
+    Facade for BoostTests.
+    """
+
+    _JUNIT_LOG_FORMAT = re.compile(r'--log_format=<(JUNIT|.*\|JUNIT\||.*\|JUNIT)>',
+                                   re.MULTILINE)
+
     @classmethod
-    def is_xml_report_supported(cls, executable):
+    def is_test_suite(cls, executable):
         try:
-            output = subprocess.check_output([executable, '--build_info'])
-            match = BoostTestFacade._BOOST_VERSION.search(
-                output.decode('utf-8'))
-            if not match:
-                return False
-            boost_version = pkg_resources.parse_version(match.group('version'))
-            return boost_version >= \
-                BoostTestFacade._MIN_BOOST_VERSION_WITH_XML_SUPPORT
+            output = subprocess.check_output([executable, '--help'],
+                                             stderr=subprocess.STDOUT,
+                                             universal_newlines=True)
         except (subprocess.CalledProcessError, OSError):
             return False
+        else:
+            return '--output_format' in output \
+                   and NewBoostTestFacade._JUNIT_LOG_FORMAT.search(output)
+
+    def list_tests(self, executable):
+        output = subprocess.check_output([executable, '--list_content=HRF'],
+                                         stderr=subprocess.STDOUT,
+                                         universal_newlines=True)
+
 
     def run_test(self, executable, test_id, test_args=()):
 
